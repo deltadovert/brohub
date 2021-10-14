a5937047a1457e8764f36d92db6cde6b8af3e1a0
Elijah O'Leary <elij2000@gmail.com>
2021-10-14 01:24:58 -0400

    we got some stuff goin!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import {React, useState} from 'react';
 
 function App() {
+  const [openMenu, setMenuState] = useState(false);
+
+  function myFunction (a, b) {
+    let c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    console.log('this is c: ' + c)
+    return c;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-layout">
+        <div className="App-header">
+          <div className='burger-box'>
+            <MenuIcon 
+              sx={{color:'#fffff',cursor:'pointer'}} 
+              fontSize="large"
+              onClick={openMenu ? ()=>setMenuState(false) : ()=>setMenuState(true) }
+            />  
+          </div>
+          {/* <img src='https://experts.uwaterloo.ca/static/img/james-barby.jpg' className="App-logo" alt="gordon savage nuts" />
+          <img src='https://experts.uwaterloo.ca/static/img/gordon-savage.jpg' className="App-logo" alt="gordon savage nuts" />
+          <img src='https://experts.uwaterloo.ca/static/img/michael-mayer.jpg' className="App-logo" alt="gordon savage nuts" /> */}
+
+          <div className="banner">
+            <div className="bro-hub-logo">BRO HUB</div>
+            <div className="bro-hub-logo">BRO HUB</div>
+            <div className="bro-hub-logo">BRO HUB</div>
+            <div className="bro-hub-logo">BRO HUB</div>
+            <div className="bro-hub-logo">BRO HUB</div>
+            <div className="bro-hub-logo">BRO HUB</div>
+          </div>
+        </div>
+        {openMenu && <div className="App-menu-in">
+          <div className="App-menu-text">
+            Home <br/>
+            About Us
+          </div>
+        </div>}
       </header>
     </div>
   );
